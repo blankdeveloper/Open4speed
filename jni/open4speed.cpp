ab76556f9986932bd4035e372b3b0fd31a955c5e
Lubos Vonasek <lvonasek@github.com>
2014-12-30 21:51:51 +0100

    fix night mode shader

--- jni/open4speed.cpp
+++ jni/open4speed.cpp
@@ -645,7 +645,7 @@ int main(int argc, char** argv) {
     glutKeyboardUpFunc(keyboardUp);
 
     /// load data
-    loadScene("assets/tracks/winter-day");
+    loadScene("assets/tracks/winter-night");
 
     /// start loop
     getPhysics()->locked = false;
